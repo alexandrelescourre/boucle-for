ecc555d2194a9fcbe212a2d830671a6ea22d6eab
alexandrelescourre <145761493+alexandrelescourre@users.noreply.github.com>
2023-09-22 11:44:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Je vais tout d'abord declarer une variable "a" qui est égale à un entier positif 
+// Puis faire un console log de 'OKLM' autant de fois que le valeur de "a"
+
+let aBis=4;
+for(let i=0;i<aBis;i++){
+    console.log('OKLM');
+}
